eedf518e8e8520030ae7f0de4c00368005282160
Eyob <86522632+Eyob14@users.noreply.github.com>
2023-02-03 21:18:50 +0300

    Time: 169 ms (40.90%), Space: 14.6 MB (42.24%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s)-1
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                break
+        return self.checker(s, left+1, right) or self.checker(s, left, right-1)
+    
+    def checker(self, s, left, right):
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
+                
+        
