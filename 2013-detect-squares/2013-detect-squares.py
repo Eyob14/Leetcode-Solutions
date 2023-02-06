1ecf7bc6ceca181b36b967c7c8237919cbd47133
Eyob <86522632+Eyob14@users.noreply.github.com>
2023-02-06 22:30:06 +0300

    Time: 340 ms (76.65%), Space: 15.6 MB (99.59%) - LeetHub

---
+++ 2013-detect-squares/2013-detect-squares.py
@@ -0,0 +1,24 @@
+class DetectSquares:
+
+    def __init__(self):
+        self.counter = Counter()
+        
+
+    def add(self, point: List[int]) -> None:
+        self.counter[(point[0],point[1])] += 1
+        
+
+    def count(self, point: List[int]) -> int:
+        result = 0
+        x, y = point
+        for x0, y0 in self.counter.keys():
+            if abs(x-x0) == abs(y-y0) and abs(x-x0) != 0:
+                result += self.counter[(x0,y0)]*self.counter[(x0,y)]*self.counter[(x,y0)]
+        return result
+        
+
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
