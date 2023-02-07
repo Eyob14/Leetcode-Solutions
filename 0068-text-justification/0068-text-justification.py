9690bdeeb765f36734c8ff071ef762ff8a711587
Eyob <86522632+Eyob14@users.noreply.github.com>
2023-02-07 22:17:24 +0300

    Time: 32 ms (76.85%), Space: 14 MB (9.57%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,8 +1,9 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        lines = []
+        answer = []
         temp = [words[0]]
         count = len(words[0])
+        
         for i in range(1, len(words)):
             if count + len(words[i]) >= maxWidth:
                 j = 1
@@ -11,14 +12,13 @@ class Solution:
                         if ' ' in temp[j]:
                             temp[j] += ' '
                             count += 1
-                            j += 1
-                        else:
-                            j += 1
+                        j += 1
                         if j == len(temp):
                             j = 0
                 else:
                     temp.append(' '*(maxWidth-count))
-                lines.append(''.join(temp))
+                    
+                answer.append(''.join(temp))
                 temp = [words[i]]
                 count = len(words[i])
             else:
@@ -28,8 +28,8 @@ class Solution:
 
         temp = ''.join(temp)
         temp += ' ' * (maxWidth-count)
-        lines.append(temp)
+        answer.append(temp)
 
-        return lines
+        return answer
                     
             
