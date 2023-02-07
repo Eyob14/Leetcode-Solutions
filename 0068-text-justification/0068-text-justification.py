1342ea7bd061a717750c429046ee049105042952
Eyob <86522632+Eyob14@users.noreply.github.com>
2023-02-07 21:55:28 +0300

    Time: 35 ms (60.05%), Space: 13.9 MB (87.62%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.py
@@ -0,0 +1,35 @@
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        lines = []
+        temp = [words[0]]
+        count = len(words[0])
+        for i in range(1, len(words)):
+            if count + len(words[i]) >= maxWidth:
+                j = 1
+                if ' ' in temp:
+                    while count < maxWidth:
+                        if ' ' in temp[j]:
+                            temp[j] += ' '
+                            count += 1
+                            j += 1
+                        else:
+                            j += 1
+                        if j == len(temp):
+                            j = 0
+                else:
+                    temp.append(' '*(maxWidth-count))
+                lines.append(''.join(temp))
+                temp = [words[i]]
+                count = len(words[i])
+            else:
+                temp.append(' ')
+                temp.append(words[i])
+                count += 1 + len(words[i])
+
+        temp = ''.join(temp)
+        temp += ' ' * (maxWidth-count)
+        lines.append(temp)
+
+        return lines
+                    
+            
