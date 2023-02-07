a72abfb1de20573dff10d0a9ce8b3d3134f5bc8f
Eyob <86522632+Eyob14@users.noreply.github.com>
2023-02-07 18:19:27 +0300

    Time: 486 ms (96.80%), Space: 27 MB (84.62%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,39 +1,15 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def partition(arr, l, r):
-            x = arr[r]
-            i = l
-            for j in range(l, r):
-
-                if arr[j] <= x:
-                    arr[i], arr[j] = arr[j], arr[i]
-                    i += 1
-
-            arr[i], arr[r] = arr[r], arr[i]
-            return i
-        def kthSmallest(arr, l, r, k):
-
-            # if k is smaller than number of
-            # elements in array
-            if (k > 0 and k <= r - l + 1):
-
-                # Partition the array around last
-                # element and get position of pivot
-                # element in sorted array
-                index = partition(arr, l, r)
-
-                # if position is same as k
-                if (index - l == k - 1):
-                    return arr[index]
-
-                # If position is more, recur 
-                # for left subarray 
-                if (index - l > k - 1):
-                    return kthSmallest(arr, l, index - 1, k)
-
-                # Else recur for right subarray 
-                return kthSmallest(arr, index + 1, r, 
-                                    k - index + l - 1)
-        return kthSmallest(nums, 0, len(nums)-1, len(nums)-k+1)
+        min_, max_ = min(nums), max(nums)
+        size = abs(min_)+abs(max_)+1
         
+        array = [0]*size
         
+        for num in nums:
+            array[num-min_] += 1
+        
+        for i in range(size-1, -1, -1):
+            if k-array[i] > 0:
+                k -= array[i]
+            else:
+                return i+min_
